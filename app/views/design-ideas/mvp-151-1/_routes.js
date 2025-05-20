1a88d11cc6eef03b82704865a0bd788fa2b147a5
AlexandraAggelidis <alexandra.aggelidis@engineering.digital.dwp.gov.uk>
2025-05-20 15:48:11 +0100

    mvp-151-2-01
    - impersonation banner css updated
    - to-do list gather items added

---
+++ app/views/design-ideas/mvp-151-1/_routes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const BASE_PATH = 'design-ideas/mvp-151-1';
+const ABS_BASE_PATH = `/${BASE_PATH}`;
+
+
+
+module.exports = router;
